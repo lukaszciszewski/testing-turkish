cb31cf1f12f05b88aa1adb5eb2df5bbd15b4ed28
lukaszciszewski <Ciszewski@tkomp.pl>
2017-03-24 08:41:09 +0100

    Update III.sql
    test

--- LUKASZ_TEST/Tables/III.sql
+++ LUKASZ_TEST/Tables/III.sql
@@ -1,5 +1,5 @@
 CREATE TABLE lukasz_test.iii (
-  department_id NUMBER(4),
+  department_id NUMBER(56),
   department_name VARCHAR2(30 BYTE) NOT NULL CONSTRAINT dept_name_nn CHECK ("DEPARTMENT_NAME" IS NOT NULL),
   manager_id NUMBER(6),
   location_id NUMBER(4),
