7448f25ef1f0f7becf9b7681b9b2a3b1bc237f7d
Łukasz Ciszewski <ciszewski@tkomp.pl>
2017-03-24 08:39:21 +0100

    1 commit

---
+++ LUKASZ_TEST/Tables/III.sql
@@ -0,0 +1,7 @@
+CREATE TABLE lukasz_test.iii (
+  department_id NUMBER(4),
+  department_name VARCHAR2(30 BYTE) NOT NULL CONSTRAINT dept_name_nn CHECK ("DEPARTMENT_NAME" IS NOT NULL),
+  manager_id NUMBER(6),
+  location_id NUMBER(4),
+  dn VARCHAR2(300 BYTE)
+);
